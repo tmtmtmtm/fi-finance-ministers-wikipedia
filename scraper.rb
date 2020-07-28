b1519b8168e6d961a2efb4c701680d31bb35e9d5
Tony Bowden <tony@tmtm.com>
2020-07-28 06:51:56 +0100

    Extract WikipediaOfficeholderRow

--- scraper.rb
+++ scraper.rb
@@ -11,7 +11,7 @@ require_relative 'lib/remove_notes'
 require_relative 'lib/scraped_wikipedia_officeholders'
 require_relative 'lib/unspan_all_tables'
 require_relative 'lib/wikipedia_officeholder_page'
-require_relative 'lib/wikipedia_table_row'
+require_relative 'lib/wikipedia_officeholder_row'
 
 # The Wikipedia page with a list of officeholders
 class ListPage < WikipediaOfficeholderPage
@@ -24,47 +24,30 @@ class ListPage < WikipediaOfficeholderPage
   end
 end
 
-
 # Each officeholder in the list
-class HolderItem < WikipediaTableRow
-  field :id do
-    wikidata_ids_in(tds[1]).first
+class HolderItem < WikipediaOfficeholderRow
+  def columns
+    %w[_ name _ dates cabinet]
   end
 
-  field :name do
-    link_titles_in(tds[1]).first
-  end
-
-  field :start_date do
+  def start_date_str
     dates[0]
   end
 
-  field :end_date do
+  def end_date_str
     dates[1]
-  end
-
-  field :replaces do
-  end
-
-  field :replaced_by do
-  end
-
-  field :cabinet do
-    wikidata_ids_in(tds[4]).first
-  end
-
-  field :cabinetLabel do
-    link_titles_in(tds[4]).first
   end
 
   def empty?
     tds[0].text.to_i.zero?
   end
 
-  private
-
   def dates
-    tds[3].text.tidy.split(/\s*–\s*/).map { |str| Date::Dotted.new(str).to_ymd }
+    tds[3].text.tidy.split(/\s*–\s*/)
+  end
+
+  def dateclass
+    Date::Dotted
   end
 end
 
